02a895acc4b482503179868ba50c33d04a9a2adc
Melek Doghmen <melek.doghmen@etudiant-fst.utm.tn>
2026-07-26 23:31:33 +0100

    Add Everybody Likes Good Arrays! [A] from Codeforces

---
+++ 1777/A - Everybody Likes Good Arrays!/solution.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void solve() {
+    int n;
+    cin >>n;
+    vector <int> a(n);
+    for(int i=0;i<n;i++){
+        cin >>a[i];
+    }
+    int j=0;
+    if(n==1){
+        cout <<0<<'
+';
+    }
+    else{
+        int j=0;
+        for(int i=0;i<n-1;i++){
+    if((a[i]+a[i+1])%2==0){
+        j++;
+    }
+    
+}
+cout <<j<<'
+';}}
+ 
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--) {
+        solve();
+        }
+    return 0;
+}
