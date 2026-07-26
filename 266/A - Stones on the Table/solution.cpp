626f8874c2ceb6fdfd26dfb3e8e1cd2c2f54df94
Melek Doghmen <melek.doghmen@etudiant-fst.utm.tn>
2026-07-26 23:12:14 +0100

    Add Stones on the Table [A] from Codeforces

---
+++ 266/A - Stones on the Table/solution.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+ int main(){
+     int n,c=0;
+     cin >>n;
+     string s;
+     cin >>s;
+     for(int i=0;i<n-1;i++){
+        if (s[i]==s[i+1]){
+                c+=1;
+ 
+ 
+        }
+     }
+     cout <<c<<endl;
+     return 0;}
