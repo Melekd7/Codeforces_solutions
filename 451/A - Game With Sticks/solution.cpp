f36c593ceb7c00607f9570da9b7868ce5e422245
Melek Doghmen <melek.doghmen@etudiant-fst.utm.tn>
2026-07-26 23:32:42 +0100

    Add Game With Sticks [A] from Codeforces

---
+++ 451/A - Game With Sticks/solution.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void solve() {
+      int n,k;
+     cin>>n>>k;
+     if(min(n,k)%2==0){
+        cout <<"Malvika"<<'
+';
+     }
+     else{
+        cout <<"Akshat"<<'
+';
+     }
+    }
+ 
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    t=1;
+    //cin>>t;
+    while(t--) {
+        solve();
+        }
+    return 0;
+}
