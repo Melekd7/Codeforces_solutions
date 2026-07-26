dca472427321e57e4af78f9e068b273df52a2e2a
Melek Doghmen <melek.doghmen@etudiant-fst.utm.tn>
2026-07-26 23:09:29 +0100

    Add Yet Another Two Integers Problem [A] from Codeforces

---
+++ 1409/A - Yet Another Two Integers Problem/solution.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+ 
+int t;
+long long a,b;
+long long d;
+ 
+cin >>t;
+while(t--){
+        cin>> a>>b;
+        d=llabs(a-b);
+ 
+ 
+ 
+ 
+ 
+        cout << (d+9)/10 <<endl;}
+ 
+ 
+ 
+ 
+ 
+ 
+return 0;}
