d12b58935e729f7fd931efe92d32615fd570d5cb
Melek Doghmen <melek.doghmen@etudiant-fst.utm.tn>
2026-07-26 23:10:47 +0100

    Add Minutes Before the New Year [A] from Codeforces

---
+++ 1283/A - Minutes Before the New Year/solution.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+ios::sync_with_stdio(0);
+cin.tie(0);
+int t;
+int a,b;
+cin >>t;
+while(t--){
+        cin >>a>>b;
+cout <<(23-a)*60+(60-b)<<endl;
+ 
+        }
+ 
+ 
+ 
+ 
+ 
+ 
+return 0;}
