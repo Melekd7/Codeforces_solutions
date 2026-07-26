096be40117e1ad3cdd91aa16b726c194e9f5b0b3
Melek Doghmen <melek.doghmen@etudiant-fst.utm.tn>
2026-07-26 23:12:45 +0100

    Add Word [A] from Codeforces

---
+++ 59/A - Word/solution.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+ int main(){
+ 
+     string s;
+     cin >>s;
+     int c=0;
+     for(int i=0;i<s.length();i++){
+        if (s[i]==tolower(s[i])){
+            c++;}
+        }
+     if(c>=s.length()-c){
+     for(int i=0;i<s.length();i++){
+        s[i]=tolower(s[i]);}}
+        else{for(int i=0;i<s.length();i++){
+                s[i]=toupper(s[i]);
+ 
+        }}
+ 
+ 
+ 
+ 
+ 
+     cout <<s<<endl;
+     return 0;}
