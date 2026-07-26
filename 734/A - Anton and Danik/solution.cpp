81d3bd56fcba9d11af2b26436cbaa8636a8eefdf
Melek Doghmen <melek.doghmen@etudiant-fst.utm.tn>
2026-07-26 23:11:55 +0100

    Add Anton and Danik [A] from Codeforces

---
+++ 734/A - Anton and Danik/solution.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int n;
+    string s;
+    cin >> n>>s;
+ 
+    int a=0,b=0;
+    for (char c:s) {
+        if (c=='A') a++;
+        else b++;
+    }
+    if (a>b) cout <<"Anton
+";
+    else if (a<b) cout<<"Danik
+";
+    else cout << "Friendship
+";
+    return 0;
+}
