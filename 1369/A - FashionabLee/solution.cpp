b75348868b77ab0cbc52a2d5eaf6e492dc45d722
Melek Doghmen <melek.doghmen@etudiant-fst.utm.tn>
2026-07-26 23:09:38 +0100

    Add FashionabLee [A] from Codeforces

---
+++ 1369/A - FashionabLee/solution.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+ 
+int t;
+long long a;
+ 
+ 
+cin >>t;
+while(t--){
+        cin>> a;
+        if (a%4==0){
+            cout << "YES" <<endl;
+        }
+        else{
+            cout << "NO" << endl;
+        }
+ 
+ 
+ 
+ 
+ 
+}
+ 
+ 
+ 
+ 
+ 
+ 
+return 0;}
