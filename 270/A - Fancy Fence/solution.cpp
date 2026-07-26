f0e8006edf3bdce902c593af83af4b36783caeff
Melek Doghmen <melek.doghmen@etudiant-fst.utm.tn>
2026-07-26 23:10:21 +0100

    Add Fancy Fence [A] from Codeforces

---
+++ 270/A - Fancy Fence/solution.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+ 
+int t;
+float a;
+ 
+ 
+cin >>t;
+while(t--){
+        cin>> a;
+        if ((int)(360/(180-a))==(360/(180-a))){
+           
+            cout << "YES" <<endl;}
+            else{
+                cout << "NO"<<endl;
+            }
+ 
+ 
+ 
+}
+ 
+ 
+ 
+ 
+ 
+ 
+return 0;}
