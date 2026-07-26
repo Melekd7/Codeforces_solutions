6724cf82bd158da4bb03a55b253f71c39bb51419
Melek Doghmen <melek.doghmen@etudiant-fst.utm.tn>
2026-07-26 23:20:23 +0100

    Add A Number Between Two Others [A] from Codeforces

---
+++ 2225/A - A Number Between Two Others/solution.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    long long n;
+    cin >> n;
+    long long a,b;
+ 
+    for (int i=0;i<n;i++) {
+        cin >>a>>b;
+        if (a*2==b){
+            cout <<"NO"<<endl;
+        
+ 
+    }
+ 
+else{
+    cout <<"YES"<<endl;
+}}
+    
+    return 0;
+}
